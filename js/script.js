9cc390d104938185c11be920b3694cac118875a5
Carlos Sandoval <sandovalcarlos183@gmail.com>
2021-03-07 18:23:03 -0500

    mouseover

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function iphone12Pro256() {
+    document.getElementById('iphone12ProPrice').innerHTML = "iphone 12 pro $1099";
+    document.getElementById('iphone12ProMaxPrice').innerHTML = "iphone 12 pro max $1199";
+}
+
+function iphone12Pro256Out() {
+    document.getElementById('iphone12ProPrice').innerHTML = "iphone 12 pro $999";
+    document.getElementById('iphone12ProMaxPrice').innerHTML = "iphone 12 pro max $1099";
+}
+
+function iphone12Pro512() {
+    document.getElementById('iphone12ProPrice').innerHTML = "iphone 12 pro $1299"
+    document.getElementById('iphone12ProMaxPrice').innerHTML = "iphone 12 pro max $1399"
+}
+
+function iphone12Pro512Out() {
+    document.getElementById('iphone12ProPrice').innerHTML = "iphone 12 pro $999";
+    document.getElementById('iphone12ProMaxPrice').innerHTML = "iphone 12 pro max $1099";
+}
